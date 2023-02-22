6b24ec4828e284bcd9df7fd4dd4776f04684f356
Arthur Zachow <arthur.zachow.dev@gmail.com>
2023-02-23 00:03:43 +0100

    Added color smoothing

--- Controller/interactive_audio.py
+++ Controller/interactive_audio.py
@@ -3,28 +3,49 @@ import matplotlib.animation as animation
 import audio_capture_service_pb2
 import numpy as np
 import scipy.fft
+import pandas as pd
 
 class LEDDancer():
     def __init__(self):
-        self.colors = [[148, 0, 211, 0], [75, 0, 130, 0], [0, 0, 255, 0], [0, 255, 0, 0], [255, 255, 0, 0], [255, 127, 0, 0]]
-        self.frequencies = [60, 250, 500, 2000, 4000, 6000]
-        self.window = np.zeros((17, 4))
+        self.colors = [[148, 0, 211, 0], [75, 0, 130, 0], [0, 0, 255, 0], [0, 255, 0, 0], [255, 255, 0, 0], [255, 127, 0, 0], [255, 0, 0, 0]]
+        self.frequencies = [60, 250, 500, 2000, 4000, 6000, 20000]
+        self.window = np.zeros((500))
+        print(self.window.shape)
         self.curr_packet = np.array([])
 
     def captureAudio(self, stub, controller):
         print("Received data")
-        packets = stub.StartCapture(audio_capture_service_pb2.ProcessToCapture(pid=19032))
-        self.curr_color = np.array(self.colors[0][0])
+        packets = stub.StartCapture(audio_capture_service_pb2.ProcessToCapture(pid=12988))
         for p in packets:
             buffer = np.frombuffer(p.captured_audio, dtype=np.float32)
             self.curr_packet = np.average(buffer.reshape(-1, 2), axis=1)
             yf = scipy.fft.rfft(self.curr_packet) / p.num_frames
             xf = scipy.fft.rfftfreq(p.num_frames, d=1./192000)
             strongest_freq = xf[np.argmax(yf)]
-            new_color = np.array([np.interp(strongest_freq, self.frequencies, np.take(self.colors, i, axis=1)) for i in range(4)]).reshape(1, 4)
-            self.window = np.append(self.window[1:], new_color, axis=0)
-            self.curr_color = np.mean(self.window, axis=0)
-            controller.set_color(self.curr_color)
+            self.window = np.append(self.window[1:], strongest_freq)
+            moving_avg = self.numpy_ewma_vectorized_v2(self.window, 80)
+            smoothed_freq = moving_avg[-20:].mean()
+            new_color = [np.interp(smoothed_freq, self.frequencies, np.take(self.colors, i, axis=1)) for i in range(4)]
+            #self.window = np.append(new_color, self.window[1:], axis=0)
+            #self.curr_color = np.mean(self.window, axis=0)
+            #self.window = np.vstack([self.numpy_ewma_vectorized_v2(np.take(self.window, i, axis=1), 50) for i in range(4)]).T
+            controller.set_color(new_color)
+
+    def numpy_ewma_vectorized_v2(self, data, window):
+        alpha = 2 /(window + 1.0)
+        alpha_rev = 1-(alpha)
+        n = data.shape[0]
+
+        pows = alpha_rev**(np.arange(n+1))
+
+        scale_arr = 1/pows[:-1]
+        offset = data[0]*pows[1:]
+        pw0 = alpha*alpha_rev**(n-1)
+
+        mult = data*pw0*scale_arr
+        cumsums = mult.cumsum()
+        out = offset + cumsums*scale_arr[::-1]
+        return out
 
 
 class AudioVisualizer():
